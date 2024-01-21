0877298dfce1d99b94cf43e65fea36b1484c0507
Tac Uchiha <cookiezhx@163.com>
2024-01-21 22:40:18 +0800

    Link libpython-static (But could not load dynamic libraries)

--- src/python_embed/__main__.py
+++ src/python_embed/__main__.py
@@ -3,11 +3,17 @@
 # contact: cookiezhx@163.com
 
 import sys
-from PySide6 import QtWidgets
+import requests
+
+# from PySide6 import QtWidgets
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    label = QtWidgets.QLabel("Hello World Hey Hey")
-    label.show()
-    sys.exit(app.exec())
+    print(sys.exec_prefix)
+    print(sys.prefix)
+    print(sys.path)
+    print(requests)
+    # app = QtWidgets.QApplication(sys.argv)
+    # label = QtWidgets.QLabel("Hello World Hey Hey")
+    # label.show()
+    # sys.exit(app.exec())
