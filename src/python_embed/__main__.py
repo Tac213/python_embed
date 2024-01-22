9d0e385f50992f461bb8a1c76bffb060ed90ed50
Tac Uchiha <cookiezhx@163.com>
2024-01-22 22:16:19 +0800

    Copy python3.dll to enable extension libraries loading

--- src/python_embed/__main__.py
+++ src/python_embed/__main__.py
@@ -3,17 +3,11 @@
 # contact: cookiezhx@163.com
 
 import sys
-import requests
-
-# from PySide6 import QtWidgets
+from PySide6 import QtWidgets
 
 
 if __name__ == "__main__":
-    print(sys.exec_prefix)
-    print(sys.prefix)
-    print(sys.path)
-    print(requests)
-    # app = QtWidgets.QApplication(sys.argv)
-    # label = QtWidgets.QLabel("Hello World Hey Hey")
-    # label.show()
-    # sys.exit(app.exec())
+    app = QtWidgets.QApplication(sys.argv)
+    label = QtWidgets.QLabel("Hello World Hey Hey")
+    label.show()
+    sys.exit(app.exec())
